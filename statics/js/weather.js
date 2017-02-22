1e16492ee54a3f03761e30edefab643a7ddef9f5
FlyingFish <1347894476@qq.com>
2017-02-22 15:38:07 +0800

    review the file

--- statics/js/weather.js
+++ statics/js/weather.js
@@ -4,23 +4,22 @@ app.factory('WeatherApi',function($http){
     obj.getIP = function(){
         return $http.jsonp("http://ipinfo.io/json?callback=JSON_CALLBACK");
     }
-    obj.getCurrent=function(ip){
-        var api="http://v.juhe.cn/weather/ip?format=1";
-        var APPKey="&key=dd0765af20d40a69cd17b8d0b1fb7aec&ip=";
+    obj.getCurrent = function(ip){
+        var api="http://v.juhe.cn/weather/ip?format=1"; 
+        var APPKey="&key=47f4bdd330534e7917a3cc2c756e5078&ip=";
         var cb="&callback=JSON_CALLBACK";
         return $http.jsonp(api+APPKey+ip+cb);
     };
     return obj;
 });
-
 app.controller('MainCtrl',function($scope,WeatherApi){
     $scope.Data={};
     WeatherApi.getIP().success(function(data){
-       var ip=data.ip;
-       WeatherApi.getCurrent(ip).success(function(data){
-      $scope.Data=data.result;
-      $scope.Data=data.result.future;
-     delete $scope.items[Object.keys($scope.items)[0]];
-       });
-    })
+        var ip=data.ip;
+        WeatherApi.getCurrent(ip).success(function(data){
+            $scope.Data=data.result;
+            $scope.items=data.result.future;
+            delete $scope.items[Object.keys($scope.items)[0]];
+        });
+    });
 });
